44efea7827fdb11382b4b6909abe9d863a3d4d63
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2025-03-15 13:32:24 -0600

    Script updates.

--- main.py
+++ main.py
@@ -3,42 +3,33 @@ import re
 import csv
 
 
-def main():
-    preprocess_csv(False)
-    df = pd.read_csv('payroll.csv')
-    pd.options.display.float_format = '{:.2f}'.format
-    pd.set_option('display.max_rows', None)
-
-    add_normalized_pay(df)
-
-    with open('out.txt', 'w') as out_file:
-        budgets_by_year(out_file, df)
-        budgets_by_dept(out_file, df)
-        budgets_by_borough(out_file, df)
-
-def preprocess_csv(preprocess):
-    if not preprocess:
+def preprocess_csv(enabled, input_file, output_file):
+    if not enabled:
         return
 
-    r = re.compile(r"(.+) #.*")
+    hash_tag_pattern = re.compile(r"(.+) #.*")
 
-    with open('payroll_original.csv', 'r', encoding='utf8', newline='') as original_csv:
-        with open('payroll.csv', 'w', encoding='utf8', newline='') as new_csv:
+    with open(input_file, 'r', encoding='utf8', newline='') as original_csv:
+        with open(output_file, 'w', encoding='utf8', newline='') as new_csv:
             writer = csv.writer(new_csv)
             header = next(csv.reader(original_csv))
             writer.writerow(header)
             header_idx = dict(zip(header, range(len(header))))
-            agency_idx = header_idx['Agency Name']
-            borough_idx = header_idx['Work Location Borough']
 
-            for row in csv.reader(original_csv):
-                m = re.match(r, row[agency_idx])
+            writer.writerows(map(lambda x: preprocess_csv_row(hash_tag_pattern, x, header_idx), csv.reader(original_csv)))
 
-                if m:
-                    row[agency_idx] = m.group(1)
+def preprocess_csv_row(hash_tag_pattern, row, header_idx):
+    agency_idx = header_idx['Agency Name']
+    borough_idx = header_idx['Work Location Borough']
+    hash_tag_match = re.match(hash_tag_pattern, row[agency_idx])
 
-                row[borough_idx] = row[borough_idx].upper()
-                writer.writerow(row)
+    if hash_tag_match:
+        row[agency_idx] = hash_tag_match.group(1)
+
+    row[borough_idx] = row[borough_idx].upper()
+
+    return row
+
 
 def add_normalized_pay(df):
     df['Calculated Hourly Rate'] = df['Regular Gross Paid'] / df['Regular Hours']
@@ -54,6 +45,19 @@ def budgets_by_borough(f, df):
 
 def normalized_pay(f, df):
     f.write(f"{df.groupby(['Title Description'])['Calculated Hourly Rate'].sum()}\n\n")
+
+def main():
+    preprocess_csv(False, 'payroll_original.csv', 'payroll.csv')
+    df = pd.read_csv('payroll.csv')
+    pd.options.display.float_format = '{:.2f}'.format
+    pd.set_option('display.max_rows', None)
+
+    add_normalized_pay(df)
+
+    with open('out.txt', 'w') as out_file:
+        budgets_by_year(out_file, df)
+        budgets_by_dept(out_file, df)
+        budgets_by_borough(out_file, df)
 
 
 if __name__ == '__main__':
